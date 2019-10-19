29d8f208b2d15cdb78f985cf74abc66b11600265
kanoshinnosuke <yushph@gmail.com>
2019-10-19 22:16:49 +0800

    add netlifyCMS

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,8 +2,6 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
 export default function Template ({data}) {
     const post = data.markdownRemark
