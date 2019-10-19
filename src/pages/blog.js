76cd9fbf7cc3a559522cd13fd01605f55c6da465
kanoshinnosuke <yushph@gmail.com>
2019-10-19 23:08:31 +0800

    add netlifyCMS

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,7 +8,7 @@ const BlogPage = ({data}) => (
     <Layout>
         <SEO title="Blog" />
         <h1>Latest Blogs</h1>
-        {data.allMarkdownRemark.edges.map(post => (
+        {data.allMarkdownRemark.edges.reverse().map(post => (
             <div key={post.node.id}>
                 <h3>{post.node.frontmatter.title}</h3>
                 <small>{post.node.frontmatter.author} on {post.node.frontmatter.date}</small>
